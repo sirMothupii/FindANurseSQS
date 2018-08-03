ca48b9e5b5ba2d2bf4f2dfb1e61d0ca2c8c1d461
sirMothupii <iamkeenan94@gmail.com>
2018-08-03 15:03:26 +0200

    Added NewMessageNotification class

---
+++ app/src/main/java/com/sirmothupii/findanurse/NurseRegistrations.java
@@ -0,0 +1,15 @@
+package com.sirmothupii.findanurse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NurseRegistrations extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_registrations);
+    }
+}
--- app/src/main/java/com/sirmothupii/findanurse/Statistics.java
+++ app/src/main/java/com/sirmothupii/findanurse/Statistics.java
@@ -1,7 +1,15 @@
 package com.sirmothupii.findanurse;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Statistics extends AppCompatActivity
 {
@@ -11,5 +19,45 @@ public class Statistics extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        //web view intent
+        Uri uri = Uri.parse("add power BI link here");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+
+        //the web view layout
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        //load the desired web page:
+
+        // Simplest usage: note that an exception will NOT be thrown
+        // if there is an error loading this page (see below).
+        webview.loadUrl("add power BI link here");
+
+        // Let's display the progress in the activity title bar, like the
+        // browser app does.
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient()
+        {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+            {
+                Toast.makeText(activity, "web could not be displayed! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webview.loadUrl("add power BI link here");
     }
 }
